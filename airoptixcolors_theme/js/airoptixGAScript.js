8c9f0201dffd59b8cd301d5427f119923fe955fb
Atulesh Kumar <atulesh.k@HCL.COM>
2018-12-12 18:15:04 +0530

    js files added

---
+++ airoptixcolors_theme/js/airoptixGAScript.js
@@ -0,0 +1,14 @@
+var isVignetteLoaded=false;$(document).ready(function(){$('[rel="external"]').click(function(){var pageName=ga_getExternalLinkName($(this).attr("href"));ga_trackPage(pageName);return true;});$('input.lifeStyleEle').click(function(){if($(this).is(':checked')){var elstr=$(this).attr("id")
+var elstr2=elstr.replace(/lse_/,"");var elid=parseInt(elstr2);if(elid<=30)
+ga_trackEvent("AO_Website_Homepage_Interaction","Video_Lifestyle_Statements",lifeStyleElementsGA[elid-1]);else if(elid>=31&&elid<=33)
+ga_trackEvent("AO_Website_NewToContacts","NewToContacts_CL_Benefits",lifeStyleElementsGA[elid-1]);else if(elid>=34)
+ga_trackEvent("AO_Website_NewToContacts","NewToContacts_Issues_Eyeglasses",lifeStyleElementsGA[elid-1]);}});$('._gaData').click(function(){var $this=$(this);var data=$this.metadata();if($this.is(":radio")||$this.is(":checkbox")){if($this.is(':checked')){ga_trackEvent(gaMatrix.category[data.C],gaMatrix.action[data.A],gaMatrix.label[data.L]);}}else{ga_trackEvent(gaMatrix.category[data.C],gaMatrix.action[data.A],gaMatrix.label[data.L]);}});$('._gaPage').click(function(){var data=$(this).metadata();ga_trackPage(data.A);});$('._gaDataValue').click(function(){var $this=$(this);var data=$this.metadata();if($this.is(":radio")||$this.is(":checkbox")){if($this.is(':checked')){ga_trackEventValue(gaMatrix.category[data.C],gaMatrix.action[data.A],gaMatrix.label[data.L],gaMatrix.value[data.V]);}}else{ga_trackEventValue(gaMatrix.category[data.C],gaMatrix.action[data.A],gaMatrix.label[data.L],gaMatrix.value[data.V]);}});$('._gaDataDefault').click(function(){var $this=$(this);var data=$this.metadata();if($this.is(":radio")||$this.is(":checkbox")){if($this.is(':checked')){ga_trackEventValue(data.C,data.A,data.L,data.V);}}else{ga_trackEventValue(data.C,data.A,data.L,data.V);}});});function ga_trackEventValue(category,action,optional_label,optional_value)
+{try{pageTracker._trackEvent(category,action,optional_label,optional_value);}catch(err){}}
+function ga_trackEvent(category,action,optional_label)
+{try{pageTracker._trackEvent(category,action,optional_label);}catch(err){}}
+function ga_trackPage(pageName)
+{try{pageTracker._trackPageview(pageName);}catch(err){}}
+function ga_getExternalLinkName(url)
+{var str=url;str=str.replace(/http:\/\//i,"");str=str.replace(/www\./i,"");str="External_URL_"+str;return str;}
+function ga_pageTracker(url)
+{var pageName=ga_getExternalLinkName(url);ga_trackPage(pageName);}
---
+++ airoptixcolors_theme/js/module.js
@@ -0,0 +1,13 @@
+$(function()
+{(function($)
+{$('.accordion > dd').hide();$('.accordion > dt.expanded').next().show();$('.accordion > dt').click(function()
+{ga_trackEvent('ao_faq_click','Clicks','ao_faq_click_all');$(this).next().slideToggle();if($(this).hasClass('expanded'))
+{$(this).addClass('collapsed').removeClass('expanded');}
+else
+{$(this).addClass('expanded').removeClass('collapsed');}
+return false;});})(jQuery);function scrollDown()
+{$("html, body").animate({scrollTop:(400)},1500);}});var home_content=$('.home-con').addClass('cloned-after').clone();$('.home-con').hide();function moveHomeCon(){if(device.isTab||device.isMobile){$('.products-area').prepend(home_content.addClass('cloned-before'));$('.products-area').find('.home-con:not(".cloned-before")').remove();}else{$('.products-area .lenses').before(home_content.addClass('cloned-after'));$('.products-area').find('.home-con:not(".cloned-after")').remove();}}
+moveHomeCon();$(window).resize(function(){moveHomeCon();});function youTubeResize(){var vidHeight=window.innerHeight||document.documentElement.clientHeight;if(vidHeight<=690){vidHeight=vidHeight-330;videowidth=Math.floor((vidHeight*4)/3);$('.iframe-video-overlay').css('height',vidHeight+'px');$('.iframe-video-overlay').css('width',videowidth+'px');}else{if($('.iframe-video-overlay').hasClass('multifocal-video'))
+vidHeight=vidHeight-340;else
+vidHeight=vidHeight-300;videowidth=Math.floor((vidHeight*4)/3);$('.iframe-video-overlay').css('height',vidHeight+'px');$('.iframe-video-overlay').css('width',videowidth+'px');}}
+setInterval(youTubeResize,100);$('.opens-video').click(function(){$(".iframe-video-overlay").append("<div id='iPlayerDesk' class='iPlayerDesk'></div>");onYouTubePlayerAPIReady();});

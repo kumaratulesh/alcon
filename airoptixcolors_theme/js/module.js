0e8926a1897b5f08372aa2a95016921e6de6cedb
Atulesh Kumar <atulesh.k@HCL.COM>
2018-12-12 19:13:02 +0530

    updated JS and tpl

--- airoptixcolors_theme/js/module.js
+++ airoptixcolors_theme/js/module.js
@@ -1,13 +1,11 @@
 (function($) {
-$(function()
-{(function($)
-{$('.accordion > dd').hide();$('.accordion > dt.expanded').next().show();$('.accordion > dt').click(function()
+$('.accordion > dd').hide();$('.accordion > dt.expanded').next().show();$('.accordion > dt').click(function()
 {ga_trackEvent('ao_faq_click','Clicks','ao_faq_click_all');$(this).next().slideToggle();if($(this).hasClass('expanded'))
 {$(this).addClass('collapsed').removeClass('expanded');}
 else
 {$(this).addClass('expanded').removeClass('collapsed');}
-return false;});})(jQuery);function scrollDown()
-{$("html, body").animate({scrollTop:(400)},1500);}});var home_content=$('.home-con').addClass('cloned-after').clone();$('.home-con').hide();function moveHomeCon(){if(device.isTab||device.isMobile){$('.products-area').prepend(home_content.addClass('cloned-before'));$('.products-area').find('.home-con:not(".cloned-before")').remove();}else{$('.products-area .lenses').before(home_content.addClass('cloned-after'));$('.products-area').find('.home-con:not(".cloned-after")').remove();}}
+return false;});;function scrollDown()
+{$("html, body").animate({scrollTop:(400)},1500);}var home_content=$('.home-con').addClass('cloned-after').clone();$('.home-con').hide();function moveHomeCon(){if(device.isTab||device.isMobile){$('.products-area').prepend(home_content.addClass('cloned-before'));$('.products-area').find('.home-con:not(".cloned-before")').remove();}else{$('.products-area .lenses').before(home_content.addClass('cloned-after'));$('.products-area').find('.home-con:not(".cloned-after")').remove();}}
 moveHomeCon();$(window).resize(function(){moveHomeCon();});function youTubeResize(){var vidHeight=window.innerHeight||document.documentElement.clientHeight;if(vidHeight<=690){vidHeight=vidHeight-330;videowidth=Math.floor((vidHeight*4)/3);$('.iframe-video-overlay').css('height',vidHeight+'px');$('.iframe-video-overlay').css('width',videowidth+'px');}else{if($('.iframe-video-overlay').hasClass('multifocal-video'))
 vidHeight=vidHeight-340;else
 vidHeight=vidHeight-300;videowidth=Math.floor((vidHeight*4)/3);$('.iframe-video-overlay').css('height',vidHeight+'px');$('.iframe-video-overlay').css('width',videowidth+'px');}}
